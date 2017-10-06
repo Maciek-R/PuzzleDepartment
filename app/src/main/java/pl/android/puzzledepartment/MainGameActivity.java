13026edfe7e2104610afa3d8d7c243455eed164c
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2017-10-06 13:46:31 +0200

    Setting orientation to landscape. Defining entry class Activity and Renderer class

---
+++ app/src/main/java/pl/android/puzzledepartment/MainGameActivity.java
@@ -0,0 +1,67 @@
+package pl.android.puzzledepartment;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by Maciek on 2017-10-06.
+ */
+
+public class MainGameActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        final MainGameRenderer mainGameRenderer = new MainGameRenderer(this);
+
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("emulator")
+                || Build.MODEL.contains("Android SDK built for x86")));
+
+        if(supportsEs2){
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(mainGameRenderer);
+            rendererSet = true;
+        }
+        else{
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(rendererSet)
+            glSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(rendererSet)
+            glSurfaceView.onResume();
+    }
+}
---
+++ app/src/main/java/pl/android/puzzledepartment/MainGameRenderer.java
@@ -0,0 +1,45 @@
+package pl.android.puzzledepartment;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_TEST;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glEnable;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by Maciek on 2017-10-06.
+ */
+
+public class MainGameRenderer implements Renderer {
+
+    private final Context context;
+
+    public MainGameRenderer(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+//        glEnable(GL_CULL_FACE);
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+}
