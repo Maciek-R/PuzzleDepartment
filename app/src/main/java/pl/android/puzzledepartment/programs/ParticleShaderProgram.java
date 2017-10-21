95aafd4c795cf20e738afbe639bb8a86e7de85e5
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2017-10-21 21:40:04 +0200

    Shader programs for ParticleSystem

---
+++ app/src/main/java/pl/android/puzzledepartment/programs/ParticleShaderProgram.java
@@ -0,0 +1,71 @@
+package pl.android.puzzledepartment.programs;
+
+import android.content.Context;
+
+import pl.android.puzzledepartment.R;
+import pl.android.puzzledepartment.objects.particles.ParticleSystem;
+
+import static android.opengl.GLES20.GL_TEXTURE0;
+import static android.opengl.GLES20.GL_TEXTURE_2D;
+import static android.opengl.GLES20.glActiveTexture;
+import static android.opengl.GLES20.glBindTexture;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform1f;
+import static android.opengl.GLES20.glUniform1i;
+import static android.opengl.GLES20.glUniformMatrix4fv;
+
+/**
+ * Created by Maciek Ruszczyk on 2017-10-21.
+ */
+
+public class ParticleShaderProgram extends ShaderProgram{
+
+    private final int uMatrixLocation;
+    private final int uTimeLocation;
+    private final int uTextureUnitLocation;
+
+    private final int aPositionLocation;
+    private final int aColorLocation;
+    private final int aDirectionVectorLocation;
+    private final int aParticleStartTimeLocation;
+
+    public ParticleShaderProgram(Context context) {
+        super(context, R.raw.particle_vertex_shader, R.raw.particle_fragment_shader);
+
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
+        uTimeLocation = glGetUniformLocation(program, U_TIME);
+        uTextureUnitLocation = glGetUniformLocation(program, U_TEXTURE_UNIT);
+
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
+        aDirectionVectorLocation = glGetAttribLocation(program, A_DIRECTION_VECTOR);
+        aParticleStartTimeLocation = glGetAttribLocation(program, A_PARTICLE_START_TIME);
+    }
+    @Override
+    public void setUniforms(float[] matrix, float elapsedTime, int textureId) {
+        glUniformMatrix4fv(uMatrixLocation, 1, false, matrix, 0);
+        glUniform1f(uTimeLocation, elapsedTime);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureId);
+        glUniform1i(uTextureUnitLocation, 0);
+    }
+
+    @Override
+    public int getPositionAttributeLocation() {
+        return aPositionLocation;
+    }
+    @Override
+    public int getColorAttributeLocation() {
+        return aColorLocation;
+    }
+    @Override
+    public int getDirectionVectorAttributeLocation() {
+        return aDirectionVectorLocation;
+    }
+    @Override
+    public int getParticleStartTimeAttributeLocation() {
+        return aParticleStartTimeLocation;
+    }
+}
--- app/src/main/java/pl/android/puzzledepartment/programs/ShaderProgram.java
+++ app/src/main/java/pl/android/puzzledepartment/programs/ShaderProgram.java
@@ -23,13 +23,14 @@ public class ShaderProgram {
     protected static final String U_COLOR = "u_Color";
     protected static final String U_LIGHT_POS = "u_LightPos";
     protected static final String U_LIGHT_COLOR = "u_LightColor";
-
+    protected static final String U_TIME = "u_Time";
     //Attribute
     protected static final String A_POSITION = "a_Position";
     protected static final String A_COLOR = "a_Color";
     protected static final String A_NORMAL = "a_Normal";
     protected static final String A_TEXTURE_COORDINATES = "a_TextureCoordinates";
-
+    protected static final String A_DIRECTION_VECTOR = "a_DirectionVector";
+    protected static final String A_PARTICLE_START_TIME = "a_ParticleStartTime";
     protected final int program;
 
     protected ShaderProgram(Context context, int vertexShaderResourceId, int fragmentShaderResourceId) {
@@ -43,13 +44,16 @@ public class ShaderProgram {
 
     public void setUniforms(float[] matrix){}
     public void setUniforms(float[] matrix, float r, float g, float b){}
-    public void setUniforms(float[] modelMatrix, float[] invertedModelMatrix, float[] modelViewProjectionMatrix, Light light) {};
+    public void setUniforms(float[] modelMatrix, float[] invertedModelMatrix, float[] modelViewProjectionMatrix, Light light) {}
     public void setUniforms(float[] modelMatrix, float[] invertedModelMatrix, float[] modelViewProjectionMatrix, Light light, float red, float green, float blue) {}
+    public void setUniforms(float[] viewProjectionMatrix, float elapsedTime, int textureId){}
     public int getPositionAttributeLocation() {
        return -1;
     }
-    public int getColorAttributeLocation() {
-       return -1;
-    }
+    public int getColorAttributeLocation() { return -1;}
     public int getNormalAttributeLocation() { return -1; }
+    public int getDirectionVectorAttributeLocation() { return -1; }
+    public int getParticleStartTimeAttributeLocation() {
+        return -1;
+    }
 }
