bff2b1074771dcc4b4e8fef9f98bf947d9f67898
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2017-10-29 19:47:28 +0100

    Applying level of deteils in multilexturing - faster rendering

--- app/src/main/res/raw/heightmap_fragment_shader.glsl
+++ app/src/main/res/raw/heightmap_fragment_shader.glsl
@@ -11,16 +11,16 @@ varying vec2 v_PassTextureCoordinates;
 
 void main()
 {
-    vec2 tiledTextureCoordinates = v_PassTextureCoordinates * 64.0;
+    vec2 tiledTextureCoordinates = v_PassTextureCoordinates * 32.0;
 
-    vec4 blendTextureColour = texture2D(u_BlendMapTextureUnit, v_PassTextureCoordinates);
+    vec4 blendTextureColour = texture2D(u_BlendMapTextureUnit, v_PassTextureCoordinates, 2.0f);
 
     float restColour = 1.0f - (blendTextureColour.r + blendTextureColour.g + blendTextureColour.b);
 
-    vec4 background = texture2D(u_BackgroundTextureUnit, tiledTextureCoordinates) * restColour;
-    vec4 redTexture = texture2D(u_RedTextureUnit, tiledTextureCoordinates) * blendTextureColour.r;
-    vec4 greenTexture = texture2D(u_GreenTextureUnit, tiledTextureCoordinates) * blendTextureColour.g;
-    vec4 blueTexture = texture2D(u_BlueTextureUnit, tiledTextureCoordinates) * blendTextureColour.b;
+    vec4 background = texture2D(u_BackgroundTextureUnit, tiledTextureCoordinates, 2.0f) * restColour;
+    vec4 redTexture = texture2D(u_RedTextureUnit, tiledTextureCoordinates, 2.0f) * blendTextureColour.r;
+    vec4 greenTexture = texture2D(u_GreenTextureUnit, tiledTextureCoordinates, 2.0f) * blendTextureColour.g;
+    vec4 blueTexture = texture2D(u_BlueTextureUnit, tiledTextureCoordinates, 2.0f) * blendTextureColour.b;
 
     vec4 totalColour = background + redTexture + greenTexture + blueTexture;
 
